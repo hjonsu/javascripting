93a734bb33dfa5f0cb801e2cb131afaae51179b8
Jonathan Su <jsu6898@gmail.com>
2021-10-09 16:35:49 -0700

    initial commit with all my work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,6 @@
+let pets = ['cat', 'dog', 'rat']
+let newPetsArray = [];
+for (let i = 0; i < pets.length; i++){
+newPetsArray.push(pets[i] + 's')
+}
+console.log(newPetsArray)
---
+++ numbers.js
@@ -0,0 +1,2 @@
+let x = 123456789
+console.log(x)
